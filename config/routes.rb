ca110b2530e61b417d1c675c517d7b66fc1d6851
MuJing Tsai <mujingtsai@Mu-Jing.local>
2018-12-27 15:55:15 +0800

    add product page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'home' , :to => 'pages#home'
+  
+  root :to => 'pages#home'
+  get 'products/:id', :to => 'products#show'
+  get 'product/category/:id', :to => 'categories#show'
 end
