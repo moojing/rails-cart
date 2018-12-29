3bf722c976f97ed15d648d490df5143c2cd699be
MuJing Tsai <mujingtsai@Mu-Jing.local>
2018-12-30 01:28:55 +0800

    user dashboard layout when the routes has admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    layout :layout_by_resource
+
+    private
+  
+    def layout_by_resource
+      if devise_controller? && resource_name == :admin
+        "dashboard"
+      else
+        "application"
+      end
+    end
 end
