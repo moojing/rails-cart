5e32649c8ef47036e0b581e6686854460641dbf7
MuJing Tsai <mujingtsai@Mu-Jing.local>
2019-01-19 20:50:24 +0800

    finish home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
 
     def home 
-        @new_products = Product.all.order(:create_at).limit(5)
+        @new_products = Product.all.order(:created_at).limit(5)
+        @posts = Post.all.order(:created_at).limit(3)
     end 
 end
