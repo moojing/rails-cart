e753623843d42d804ca0d5e85395c78d9ffe5199
MuJing Tsai <mujingtsai@Mu-Jing.local>
2019-03-10 13:30:26 +0800

    change datatime to timestampz in addcolumnremaintocoupon

--- db/migrate/20190116174221_add_column_remain_to_coupon.rb
+++ db/migrate/20190116174221_add_column_remain_to_coupon.rb
@@ -1,6 +1,6 @@
 class AddColumnRemainToCoupon < ActiveRecord::Migration[5.2]
   def change
     add_column  :coupons , :remain , :integer , default:0
-    change_column :coupons, :expire_at , :timestampz
+    change_column :coupons, :expire_at , 'timestamptz using expire_at::timestamptz'
   end
 end
