5ac66d4d4b9cb99a8716333c089651c1743b13a2
MuJing Tsai <mujingtsai@Mu-Jing.local>
2019-03-10 12:45:47 +0800

    change addColumntoCoupon used type

--- db/migrate/20190116161437_add_column_to_coupon.rb
+++ db/migrate/20190116161437_add_column_to_coupon.rb
@@ -1,6 +1,6 @@
 class AddColumnToCoupon < ActiveRecord::Migration[5.2]
   def change
     add_column  :coupons , :code , :text , null: false,limit:10
-    change_column :coupons ,:used ,:integer
+    change_column :coupons ,:used ,'integer USING CAST(used AS integer)'
   end
 end
